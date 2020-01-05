e38b5d5cf06ef35e3cc3a504a6d9b6bb36dd8224
KyuhwanYeon <KyuhwanYeon@gmail.com>
2020-01-05 23:22:21 +0900

    Initial update

--- src/process.cpp
+++ src/process.cpp
@@ -3,7 +3,10 @@
 #include <sstream>
 #include <string>
 #include <vector>
+#include <iostream>
+#include "math.h"
 
+#include "linux_parser.h"
 #include "process.h"
 
 using std::string;
@@ -11,23 +14,167 @@ using std::to_string;
 using std::vector;
 
 // TODO: Return this process's ID
-int Process::Pid() { return 0; }
+int Process::Pid() { return pid; }
 
 // TODO: Return this process's CPU utilization
-float Process::CpuUtilization() { return 0; }
+float Process::CpuUtilization() { return cpuutilization; }
 
 // TODO: Return the command that generated this process
-string Process::Command() { return string(); }
+string Process::Command() { return command; }
 
 // TODO: Return this process's memory utilization
-string Process::Ram() { return string(); }
+string Process::Ram() { return ram; }
 
 // TODO: Return the user (name) that generated this process
-string Process::User() { return string(); }
+string Process::User() { return user; }
 
 // TODO: Return the age of this process (in seconds)
-long int Process::UpTime() { return 0; }
+long int Process::UpTime() { return uptime; }
 
 // TODO: Overload the "less than" comparison operator for Process objects
 // REMOVE: [[maybe_unused]] once you define the function
 bool Process::operator<(Process const& a[[maybe_unused]]) const { return true; }
+void Process::Pid(int numPid) { pid = numPid; }
+void Process::User(int numPid) {
+  string line;
+  string key;
+  string value;
+  string UID;
+  std::ifstream filestream(LinuxParser::kProcDirectory + to_string(numPid) +
+                           '/' + LinuxParser::kStatusFilename);
+  if (filestream.is_open()) {
+    while (std::getline(filestream, line)) {
+      std::istringstream linestream(line);
+      while (linestream >> key >> value) {
+        if (key == "Uid:") {
+          UID = value;
+        }
+      }
+    }
+  }
+   std::ifstream filestream2(LinuxParser::kPasswordPath);
+  if (filestream2.is_open()) {
+    while (std::getline(filestream2, line)) {
+      std::replace(line.begin(), line.end(), ':', ' ');
+      std::replace(line.begin(), line.end(), 'x', ' ');
+  //    std::replace(line.begin(), line.end(), '"', ' ');
+      std::istringstream linestream2(line);
+      while (linestream2 >> value>>key) {
+        if (key == UID) {
+          user = value;
+        }
+      }
+    }
+  }
+
+
+
+}
+
+void Process::Command(int numPid) {
+  string line;
+  string key;
+  string value;
+  std::ifstream filestream(LinuxParser::kProcDirectory + to_string(numPid) +
+                           '/' + LinuxParser::kCmdlineFilename);
+  if (filestream.is_open()) {
+    while (std::getline(filestream, line)) {
+      std::istringstream linestream(line);
+      command = line;
+    }
+  }
+}
+
+
+
+void Process::Ram(int numPid) {
+  string line;
+  string key;
+  string value;
+  float RAMkb;
+  int RAMmb;
+  std::ifstream filestream(LinuxParser::kProcDirectory + to_string(numPid) +
+                           '/' + LinuxParser::kStatusFilename);
+  if (filestream.is_open()) {
+    while (std::getline(filestream, line)) {
+      std::istringstream linestream(line);
+      while (linestream >> key >> value) {
+        if (key == "VmSize:") {
+           RAMkb=  std::stof(value);
+           RAMmb=floor(RAMkb/1024);  // kb to mb 
+          ram = to_string(RAMmb);
+        }
+      }
+    }
+  }
+}
+
+#define HERTZ sysconf(_SC_CLK_TCK)
+void Process::CpuUtilization(int numPid) {
+  string line;
+  string uptime;
+  float utime;
+  float stime;
+  float cutime;
+  float cstime;
+  float starttime;
+  
+  string tmp;
+  vector<string> statpos(22);
+  float fuptime;
+  string value;
+  std::ifstream filestream(LinuxParser::kProcDirectory +LinuxParser::kUptimeFilename);
+  if (filestream.is_open()) {
+    while (std::getline(filestream, line)) {
+      std::istringstream linestream(line);
+      while (linestream >> uptime>>value ) {
+          fuptime = stof(uptime);
+      }
+    }
+  }
+  std::ifstream filestream2(LinuxParser::kProcDirectory + to_string(numPid) +
+                           '/' + LinuxParser::kStatFilename);
+  if (filestream2.is_open()) {
+    while (std::getline(filestream2, line)) {
+      std::istringstream linestream2(line);
+      for (int i = 0; i < 22; i++)
+      {
+          linestream2 >> tmp;
+          statpos[i]=tmp;
+      }
+      utime = stof(statpos[13]);
+      stime = stof(statpos[14]);
+      cutime = stof(statpos[15]);
+      cstime = stof(statpos[16]);
+      starttime = stof(statpos[21]);  // Reference: https://stackoverflow.com/questions/16726779/how-do-i-get-the-total-cpu-usage-of-an-application-from-proc-pid-stat/16736599#16736599
+ //     Process::uptime = starttime/HERTZ;
+      cpuutilization=((utime+stime+cutime+cstime)/HERTZ)/(fuptime-(starttime/HERTZ));
+    }
+  }
+}
+
+
+void Process::UpTime(int numPid) {
+  string line;
+
+  float starttime;
+  string tmp;
+  vector<string> statpos(22);
+  string value;
+  std::ifstream filestream(LinuxParser::kProcDirectory + to_string(numPid) +
+                           '/' + LinuxParser::kStatFilename);
+  if (filestream.is_open()) {
+    while (std::getline(filestream, line)) {
+      std::istringstream linestream(line);
+      for (int i = 0; i < 22; i++)
+      {
+          linestream >> tmp;
+          statpos[i]=tmp;
+      }
+      starttime = stof(statpos[21]);  
+      uptime = starttime/HERTZ;
+      //std::cout<<uptime<<std::endl;
+      
+    }
+  }
+}
--- src/system.cpp
+++ src/system.cpp
@@ -1,12 +1,13 @@
+#include "system.h"
 #include <unistd.h>
 #include <cstddef>
+#include <iostream>
 #include <set>
 #include <string>
 #include <vector>
-
+#include "linux_parser.h"
 #include "process.h"
 #include "processor.h"
-#include "system.h"
 
 using std::set;
 using std::size_t;
@@ -17,22 +18,42 @@ using std::vector;
 Processor& System::Cpu() { return cpu_; }
 
 // TODO: Return a container composed of the system's processes
-vector<Process>& System::Processes() { return processes_; }
+vector<Process>& System::Processes() {
+  int PID;
+  vector<int> vecPID = LinuxParser::Pids();
+  int numProcess = vecPID.size();
+  vector<Process> Tmp(numProcess);
+  processes_ = Tmp;
+  for (int idxPID = 0; idxPID < numProcess; idxPID++) {
+    PID =vecPID[idxPID];
+    processes_[idxPID].Pid(PID);
+    processes_[idxPID].User(PID);
+    processes_[idxPID].Command(PID);
+    processes_[idxPID].Ram(PID);
+    processes_[idxPID].CpuUtilization(PID);
+    processes_[idxPID].UpTime(PID);
+    std::cout<<processes_[idxPID].UpTime()<<std::endl;
+  }
+
+  return processes_;
+}
 
 // TODO: Return the system's kernel identifier (string)
-std::string System::Kernel() { return string(); }
+std::string System::Kernel() {
+  std::string s = LinuxParser::Kernel();
+  return s;
+}
 
 // TODO: Return the system's memory utilization
-float System::MemoryUtilization() { return 0.0; }
+float System::MemoryUtilization() { return LinuxParser::MemoryUtilization(); }
 
 // TODO: Return the operating system name
-std::string System::OperatingSystem() { return string(); }
+std::string System::OperatingSystem() { return LinuxParser::OperatingSystem(); }
 
 // TODO: Return the number of processes actively running on the system
-int System::RunningProcesses() { return 0; }
+int System::RunningProcesses() { return LinuxParser::RunningProcesses(); }
 
 // TODO: Return the total number of processes on the system
-int System::TotalProcesses() { return 0; }
-
+int System::TotalProcesses() { return LinuxParser::TotalProcesses(); }
 // TODO: Return the number of seconds since the system started running
-long int System::UpTime() { return 0; }
+long int System::UpTime() { return LinuxParser::UpTime(); }
